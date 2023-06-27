51dfc2fe105a77ead6d992a34515405853c4bd7d
Bryan Riley <brileydeveloper@gmail.com>
2023-06-27 12:13:05 -0400

    Initial commit

---
+++ droneClient.js
@@ -0,0 +1,20 @@
+const DroneMobile = require('drone-mobile')
+const droneClient = new DroneMobile({
+  username: process.env.DRONE_MOBILE_USER,
+  password: process.env.DRONE_MOBILE_PASSWORD,
+})
+
+try {
+  droneClient.on('ready', async () => {
+    const vehicleList = await droneClient.vehicles()
+    const vehicle = vehicleList[0]
+  })
+
+  droneClient.on('error', async () => {
+    console.log('Error initiaing client')
+  })
+} catch (e) {
+  console.log('client failed for reason:', e)
+}
+
+module.exports = droneClient
---
+++ index.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const droneClient = require('./droneClient')
+
+app.get('/', async (req, res) => {
+  const vehicleList = await droneClient.vehicles()
+  const currentStatus = await droneClient.status(vehicleList[0].vehicle_name)
+  console.log('Current Status:', currentStatus)
+  const status = await droneClient.start(vehicleList[0].vehicle_name)
+
+  res.send(status)
+})
+
+app.listen(port, () => {
+  console.log('Drone Mobile service is on port:', port)
+})
